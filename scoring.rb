9b0dabc9d900b94a8b05043ba4371430d8e3d19e
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2018-10-30 22:08:21 -0500

    printing result inside of main file

--- scoring.rb
+++ scoring.rb
@@ -2,7 +2,9 @@
 
 require './lib/bowling'
 require './lib/opts_parser'
+require 'colorize'
 
 # Init game based in the input
 game = Bowling::Game.new(OPTIONS[:pattern])
-game.final_score
+# Print result
+game.final_score.each { |k, v| puts "#{k} is #{v}".light_green }
