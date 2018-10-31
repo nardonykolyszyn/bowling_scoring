eab7a1c76ad6bb60ec83891a0542d8c6622e7ac8
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2018-10-30 22:47:44 -0500

    add new specs

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -5,9 +5,10 @@ require './lib/bowling'
 
 describe Bowling do
   let(:perfect_game) { Bowling::Game.new('XXXXXXXXXXXX') }
+  let(:game_with_spares) { Bowling::Game.new('5/5/5/5/5/5/5/5/5/5/5') }
+  let(:game_with_gutters) { Bowling::Game.new('9-9-9-9-9-9-9-9-9-9-') }
 
   # Perfect game specs
-
   it 'should return 300 as score' do
     expect(perfect_game.score.inject(:+)).to eq 300
   end
@@ -18,5 +19,27 @@ describe Bowling do
 
   it 'should return an exception' do
     expect { Bowling::Game.new }.to raise_error(ArgumentError)
+  end
+
+  # Game with spares specs
+
+  it 'should return 150 as score' do
+    expect(game_with_spares.score.inject(:+)).to eq 150
+  end
+
+  # Game with gutters
+
+  it 'should return 90 as score' do
+    expect(game_with_gutters.score.inject(:+)).to eq 90
+  end
+
+  # Arguments specs
+
+  it 'should reject if game has more than 12 pairs' do
+    expect { Bowling::Game.new('9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-') }.to raise_error(ArgumentError)
+  end
+
+  it 'should raise error if takes more than 12 strikes' do
+    expect { Bowling::Game.new('XXXXXXXXXXXXX') }.to raise_error(ArgumentError)
   end
 end
