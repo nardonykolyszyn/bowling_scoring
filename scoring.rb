1da28326e76338783fb238f85c04e753cc858733
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2018-10-30 20:32:57 -0500

    call to score board model as instance method

--- scoring.rb
+++ scoring.rb
@@ -4,4 +4,5 @@ require './lib/bowling'
 require './lib/opts_parser'
 
 # Init game based in the input
-Bowling::Game.new(OPTIONS[:pattern])
+game = Bowling::Game.new(OPTIONS[:pattern])
+game.final_score
