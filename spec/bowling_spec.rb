cef0104dd6003c4d27c3b25b6f86b799f212f23b
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2018-10-30 20:33:10 -0500

    add tests

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,7 +1,22 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require './lib/bowling'
 
 describe Bowling do
-  let(:game) { Bowling.new }
+  let(:perfect_game) { Bowling::Game.new('XXXXXXXXXXXX') }
+
+  # Perfect game specs
+
+  it 'should return 300 as score' do
+    expect(perfect_game.score.inject(:+)).to eq 300
+  end
+
+  it 'should return 12 as pairs' do
+    expect(perfect_game.frames.bowls.size).to eq 12
+  end
+
+  it 'should return an exception' do
+    expect { Bowling::Game.new }.to raise_error(ArgumentError)
+  end
 end
