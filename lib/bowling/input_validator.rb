ee2d6008020e8b036866aba754beeb90f55efc56
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2018-10-30 22:47:35 -0500

    validates max number of strikes

--- lib/bowling/input_validator.rb
+++ lib/bowling/input_validator.rb
@@ -10,12 +10,17 @@ module InputValidator
   def self.extract_value(input)
     extract_numbers_validation(input)
     extract_special_characters_validation(input)
+    check_max_of_strikes(input)
     input.scan(/.{1,2}/)
   end
 
   # Validations
   def self.extract_numbers_validation(input)
     raise ArgumentError, 'Please check your data' if input.scan(/\d/).size > 12
+  end
+
+  def self.check_max_of_strikes(input)
+    raise ArgumentError, 'Please check your data' if input.count('X') > 12
   end
 
   def self.extract_special_characters_validation(input)
